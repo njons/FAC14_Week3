a61ba0fa7118f04c6b49083bf9b79505f777ed3e
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-07-28 14:20:08 +0100

    added dependencies and started server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
+// require http module
+const http = require('http');
+// require routes to redirect server requests from http
+const routes = require('./route')
+
+
 // make a server
+const server = http.createServer(routes);
+// setup the port (heroku style) to allow heroku to pick a port and set a local one at 3000
+const port = process.env.PORT || 3000;
+
+server.listen(port , () => {
+  console.log(`Server is up and runing on port ${port}`)
+})
