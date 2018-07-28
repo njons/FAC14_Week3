c1c47a0bcb70b00d8bd5792c994f3bc2576c2c89
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-07-28 14:48:00 +0100

    added routes and 404

--- src/route.js
+++ src/route.js
@@ -1,7 +1,20 @@
+// link to handler.js
+const handler = require('./handler');
 
 const route = (request, response) => {
-  
-}
+  const url = request.url;
 
+  if (url === "/") {
+    handler.homeRoute(request, response);
+  } else if (url.includes === "public") {
+    handler.publicRoute(request, response);
+  } else if (url.includes === "search/"){
+    handler.queryRoute(request, response);
+  } else {
+    response.writeHead(404, {"Content-Type" : "text/html"});
+    response.end(`this is the custom 404, right here <3`)
+  }
+};
 
+// make variable accessible to server.js
 module.exports = route;
