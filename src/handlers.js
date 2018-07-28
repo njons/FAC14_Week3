99ae15b210e7515560f34816ff075d7f42c431f3
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-07-28 17:02:55 +0100

    add publicRoute

--- src/handlers.js
+++ src/handlers.js
@@ -5,7 +5,7 @@ const path = require('path');
 // const queryString = require('querystring');
 
 const homeRoute = (request, response) => {
-  // when a response goes through - this one is always the index so the content type can be set
+  // when files are found - this one is always the index so the content type can be set
   response.writeHead(200, {"Content-Type":"text/html"});
   // read the file
   fs.readFile(
@@ -13,7 +13,8 @@ const homeRoute = (request, response) => {
     path.join(__dirname, "..", "public", "index.html"),
     (error, file) => {
       if (error) {
-        respose.writeHead(500, `Internal Server Error: ${error}`);
+        respose.writeHead(500, {"Content-Type":"text/html"});
+        response.end(`<h1>Somethisng went wrong on our end!</h1>`);
       } else {
         response.end(file);
       }
@@ -21,8 +22,28 @@ const homeRoute = (request, response) => {
   );
 }
 
-function publicRoute(request, response){
+function publicRoute(request, response, url){
+  // make sure you can handle multiple "Content-Type" for each of the files
+  const extension = url.split(".")[1];
+  // write an object to hold all of the different "Content-Type"s in the public folder
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript"
+  };
 
+  // when files come through
+  response.writeHead(200, {"Content-Type" : "text/html" })
+
+  fs.readFile(
+    path.join(__dirname, '..', 'public', url), (error, file) => {
+      if (error) {
+        response.writeHead
+      } else {
+        response.end(file);
+      }
+    }
+  )
 }
 
 function queryRoute(request, response) {
