8fc19baffd390aa55f19d987e7892f7ac68b72c8
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-18 23:25:46 +0100

    added styling template and gifs

--- public/js/script.js
+++ public/js/script.js
@@ -2,39 +2,32 @@ var input = document.getElementById('search');
 var button = document.getElementById('submit');
 var datalist = document.getElementById('datalist');
 
-// create an object ready to hold the final filtered object with rearch results
-var globalObj = {};
 
-var searchTermSanitised
-var giphy_api_key = "7cWhgewjrS2GSML9Sj3iCQZd24zYFLSh";
-var giphy_url = "https://api.giphy.com/v1/gifs/random?tag=" + searchTermSanitised + "&api_key=" + giphy_api_key;
-
-button.addEventListener('submit', function(event) {
-  event.preventDefault();
-  xhrRequest('api.giphy.com/v1/gifs/trending/'
-api_key: string, '')
-
-})
-
-// create a URL based on the input from the user
-function urlCreator (url, str) {
-  // takes url and adds a string value that corresponds to the user search input (str)
-  var searchUri = encodeURIComponent(str);
-  console.log('this is the url creaded with the user input:', searchUri)
-  return url + "search/" + searchUri;
+// random number generator
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+var giphy_api_key = "7cWhgewjrS2GSML9Sj3iCQZd24zYFLSh";
+var giphy_url = "https://api.giphy.com/v1/gifs/trending?&api_key=" + giphy_api_key;
+// var giphy_url = "https://api.giphy.com/v1/gifs/random?tag=" + searchTermSanitised + "&api_key=" + giphy_api_key;
+console.log('this is the trendy url', giphy_url)
+
 
 // the generic xhr request wrapped in a function
 function xhrRequest(url, cb) {
   // create the new instance of a request object
   var xhr = new XMLHttpRequest(url, cb);
+  // console.log('this is xhr:', xhr)
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       // ensure that the object returned form the server is parsed
       var parsedObj = JSON.parse(xhr.responseText);
-      // transfer the results into the empty object waiting for info
-      globalObj = parsedObj;
-      console.log('this is the parsed obj', globalObj)
+      console.log('this is the parsed data', parsedObj)
+      // // transfer the results into the empty object waiting for info
+      // globalObj = parsedObj;
+      // console.log('this is the parsed obj', globalObj)
       return cb(parsedObj);
     } else if (xhr.readyState === 4 && xhr.status != 200){
       // logs error if request gor all the way, but a status code other than 200 is returned
@@ -45,6 +38,33 @@ function xhrRequest(url, cb) {
   xhr.open("GET", url, true);
   xhr.send();
 }
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log('you clicked the button')
+  // call the
+  xhrRequest(giphy_url, function(data) {
+    var i = getRandomInt(0,25)
+    
+    console.log(data.data[i].images.downsized_medium.url)
+    var results = document.getElementById('results');
+    var img = document.createElement('img')
+    img.setAttribute('src', data.data[i].images.downsized_medium.url);
+    results.appendChild(img);
+  })
+
+
+})
+
+// // create a URL based on the input from the user
+// function urlCreator (url, str) {
+//   // takes url and adds a string value that corresponds to the user search input (str)
+//   var searchUri = encodeURIComponent(str);
+//   console.log('this is the url creaded with the user input:', searchUri)
+//   return url + "search/" + searchUri;
+// }
+
+
 
 // function createResultArrayCb(obj) {
 //   console.log('this is the returning obj:', obj)
